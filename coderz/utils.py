d6c36024c68ff03fd392e420d6867d7c59ac78de
Maayan Blum <maayanblm@gmail.com>
2019-07-01 09:38:34 +0300

    pythonic camel case to snake case

--- coderz/utils.py
+++ coderz/utils.py
@@ -1,6 +1,13 @@
-import re
 
 
+# convert javascript style variable names to python style.
+# the suggested solution uses re which fails to work on the javascript python compiler:
+# **************************************************************
+# import re
+# def camel_case_to_snake_case(name):
+#     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+#     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+# **************************************************************
+# hence I cooked up this magnificent pythonic alternative:
 def camel_case_to_snake_case(name):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+    return "".join(["_" + letter.lower() if letter.isupper() and i > 0 else letter.lower() for i, letter in enumerate(name)])
