ba587b6c5f4a9bddb4e620d75cbc2d201c281446
Mohammad Qashoa <mqashoa@intelitek.com>
2020-01-22 14:16:55 +0200

    new version: 0.1.4

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("Readme.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='coderz',
-    version='0.1.3',
+    version='0.1.4',
     author="CoderZ",
     author_email="app@gocoderz.com",
     python_requires='>3.0',
     description="CoderZ External IDE Tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    #url="https://github.com/javatechy/dokr",
+    url="https://github.com/go-coderz/coderz-python",
     install_requires=[
     'websockets>=7.0',
     'python-socketio==4.0.2',
