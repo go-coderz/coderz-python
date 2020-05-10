0984bea17e4078d66ca8cc11cf8b02bea126cb11
Maayan Blum <maayanblm@gmail.com>
2020-05-10 17:35:47 +0300

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("Readme.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='coderz',
-    version='0.1.8',
+    version='0.1.9',
     author="CoderZ",
     author_email="app@gocoderz.com",
     python_requires='>3.0',
